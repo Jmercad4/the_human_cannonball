b610654a772b008cf8112713daac9ece934df7b4
ttran244 <ttran244@uwo.ca>
2015-03-29 03:32:10 -0400

    adding menu images

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,16 @@
 ajtxz_hcgame.menu = function () {
     var game = ajtxz_hcgame.game;
 
+    // Use background.jpg
     // game.add.image(0, 0, 'menu_background');
-    var title = game.add.image(game.world.centerX, game.world.centerY - 150, 'menu_title');
+    
+    var title = game.add.image(game.world.centerX, game.world.centerY - 400, 'menu_title');
     title.anchor.setTo(0.5, 0.5);
-    var start = game.add.button(game.world.centerX, game.world.centerY, 'menu_start',this.playGame);
+    var start = game.add.button(game.world.centerX, game.world.centerY - 200, 'menu_start',this.playGame);
     start.anchor.setTo(0.5, 0.5);
-    // var level = game.add.button(game.world.centerX, game.world.centerY + 100, 'menu_level');
-    //level.anchor.setTo(0.5, 0.5);
-    var instr = game.add.button(game.world.centerX, game.world.centerY + 200, 'menu_instr');
+    var level = game.add.button(game.world.centerX, game.world.centerY - 100, 'menu_level');
+    level.anchor.setTo(0.5, 0.5);
+    var instr = game.add.button(game.world.centerX, game.world.centerY, 'menu_instr');
     instr.anchor.setTo(0.5, 0.5);
 
 
